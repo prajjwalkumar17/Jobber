87bfcc51d8d783126581bcd622a350d9fabdfc02
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-06-22 11:47:31 +0530

    init

---
+++ app/src/main/java/com/rejointech/jobber/Startup/Splash.java
@@ -0,0 +1,16 @@
+package com.rejointech.jobber.Startup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rejointech.jobber.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}

0fd63c93f9ee442b303b305f2db5223591677a1f
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-06-24 11:33:54 +0530

    in the making of home frag

--- app/src/main/java/com/rejointech/jobber/Containers/HomeContainer.java
+++ app/src/main/java/com/rejointech/jobber/Containers/HomeContainer.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.rejointech.jobber.Fragments.Home.homeFragment;
 import com.rejointech.jobber.R;
+import com.rejointech.jobber.Startup.splash;
 
 public class HomeContainer extends AppCompatActivity {
 
@@ -12,5 +14,10 @@ public class HomeContainer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        addfirstFragment4User();
+    }
+
+    private void addfirstFragment4User() {
+            getSupportFragmentManager().beginTransaction().add(R.id.startupViewContainer,new homeFragment()).commit();
     }
 }
